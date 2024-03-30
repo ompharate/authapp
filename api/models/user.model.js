834f13a9c2ab49955f6355f61b66cf986942830f
ompharate31 <ompharate31@gmail.com>
2024-03-30 22:47:54 +0530

    created uer model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+const User = model("user",userSchema);
+export default User;
