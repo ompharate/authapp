3b00447f14289d977777bc2e71fea7ef9a474d64
ompharate31 <ompharate31@gmail.com>
2024-03-30 23:53:37 +0530

    complete signup ui

--- client/src/componentes/Header.jsx
+++ client/src/componentes/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     <div className="bg-sky-500">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold">Auth app</h1>
+          <h1 className="font-bold text-white">Auth app</h1>
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-4 text-white">
           <Link to="/">
             <li>Home</li>
           </Link>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="bg-slate-100 p-3"
+          type="text"
+          placeholder="Username"
+          name="username"
+        />
+        <input
+          className="bg-slate-100 p-3"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          className="bg-slate-100 p-3"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80 font-bold">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/signin">
+          <span className="text-blue-500">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
