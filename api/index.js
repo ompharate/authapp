7261a0143cf49df5675559f3bb8df2a6c0608a85
ompharate31 <ompharate31@gmail.com>
2024-03-30 23:02:37 +0530

    created api route setup

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
-import express from "express"
+import express from "express";
 import connectToDb from "../services/database/connection.js";
 import dotenv from "dotenv";
 
+import userRoute from "./routes/user.route.js";
 const app = express();
 dotenv.config();
 
 connectToDb();
-app.listen(3000,()=>{
-    console.log("http://localhost:3000");
-})
+
+app.use("/api/user",userRoute);
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000");
+});
