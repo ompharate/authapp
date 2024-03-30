6deb6a620aba13e8c583ff2fe73a483264f434f8
ompharate31 <ompharate31@gmail.com>
2024-03-30 22:09:12 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<Signup/>}/>
 
-  return (
-    <>
-     <h1 className='text-red-600'>hello word</h1>
-    </>
-  )
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1 className='text-red-700'>
+        Home file
+    </h1>
+  )
+}
+
+export default Home
